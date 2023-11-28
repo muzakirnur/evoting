0b80fcb563ff5965e1b89872de9bb396ecd0fa41
Muzakir Nur <muzakir.nurr@gmail.com>
2023-11-28 10:37:29 +0700

    Perbaikan Login

--- resources/views/layouts/authentication.blade.php
+++ resources/views/layouts/authentication.blade.php
@@ -24,41 +24,32 @@
     </head>
     <body class="font-inter antialiased bg-slate-100 dark:bg-slate-900 text-slate-600 dark:text-slate-400">
 
-        <main class="bg-white">
+        <main class="bg-[url('../../public/images/bg-login.jpg')] bg-blend-multiply bg-center bg-no-repeat">
 
-            <div class="relative flex">
+            <div class="relative">
 
                 <!-- Content -->
-                <div class="w-full md:w-1/2">
+                <div class="w-full">
 
                     <div class="min-h-screen h-full flex flex-col after:flex-1">
 
                         <!-- Header -->
                         <div class="flex-1">
-                            <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-8">
+                            <div class="flex items-center justify-between h-16 px-4 sm:px-6 lg:px-4">
                                 <!-- Logo -->
                                 <a class="block" href="{{ route('dashboard') }}">
-                                    <svg width="32" height="32" viewBox="0 0 32 32">
-                                        <defs>
-                                            <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
-                                                <stop stop-color="#A5B4FC" stop-opacity="0" offset="0%" />
-                                                <stop stop-color="#A5B4FC" offset="100%" />
-                                            </linearGradient>
-                                            <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
-                                                <stop stop-color="#38BDF8" stop-opacity="0" offset="0%" />
-                                                <stop stop-color="#38BDF8" offset="100%" />
-                                            </linearGradient>
-                                        </defs>
-                                        <rect fill="#6366F1" width="32" height="32" rx="16" />
-                                        <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
-                                        <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
-                                        <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-                                    </svg>
+                                    <div class="p-5 flex flex-wrap gap-1">
+                                        <img src="{{ asset('images/langkat.png') }}" alt="Kabupaten Langkat" class="w-14 rounded-lg">
+                                        <div class="">
+                                            <h1 class="font-semibold text-white text-xl">Kapubaten</h1>
+                                            <h1 class="font-semibold text-white text-xl">Langkat</h1>
+                                        </div>
+                                    </div>
                                 </a>
                             </div>
                         </div>
 
-                        <div class="w-full max-w-sm mx-auto px-4 py-8">
+                        <div class="w-full max-w-md mx-auto px-4 py-8 h-full">
                             {{ $slot }}
                         </div>
 
@@ -67,10 +58,10 @@
                 </div>
 
                 <!-- Image -->
-                <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
+                {{-- <div class="hidden md:block absolute top-0 bottom-0 right-0 md:w-1/2" aria-hidden="true">
                     <img class="object-cover object-center w-full h-full" src="{{ asset('images/auth-image.jpg') }}" width="760" height="1024" alt="Authentication image" />
                     <img class="absolute top-1/4 left-0 -translate-x-1/2 ml-8 hidden lg:block" src="{{ asset('images/auth-decoration.png') }}" width="218" height="224" alt="Authentication decoration" />
-                </div>
+                </div> --}}
 
             </div>
 
