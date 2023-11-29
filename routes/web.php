d6b21370e9e0da8a986918ff4de575a085d0cd08
Muzakir Nur <muzakir.nurr@gmail.com>
2023-11-29 10:11:44 +0700

    Adding Index for Jadwal Pemilihan

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('jadwal', function(){
+        return view('pages.jadwal.index');
+    })->name('jadwal.index');
     Route::fallback(function() {
         return view('pages/utility/404');
     });    
